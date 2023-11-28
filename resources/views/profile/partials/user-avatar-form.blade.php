49fe514f18ebe4a713b08dda4f62920b3baba7ca
Merdan <usersuper081@gmail.com>
2023-11-29 01:55:21 +0500

    1.1.0

--- resources/views/profile/partials/user-avatar-form.blade.php
+++ resources/views/profile/partials/user-avatar-form.blade.php
@@ -3,6 +3,9 @@
         <h2 class="text-lg font-medium text-gray-900">
             User Avatar
         </h2>
+
+        <img width="50" height="50" class="rounded-full" src="{{"/storage/$user->avatar"}}" alt="user avatar" />
+
         <p class="mt-1 text-sm text-gray-600">
             Add or update user avatar
         </p>
@@ -17,13 +20,13 @@
 
 
 
-    <form method="post" action="{{ route('profile.avatar') }}">
+    <form method="post" action="{{ route('profile.avatar') }}" enctype="multipart/form-data">
         @method('patch')
         @csrf
 
         <div>
             <x-input-label for="name" value="Avatar" />
-            <x-text-input id="avatar" name="avatar" type="file" class="mt-1 block w-full" :value="old('avatar', $user->avatar)" required autofocus autocomplete="avatar" />
+            <x-text-input id="avatar" name="avatar" type="file" class="mt-1 block w-full" :value="old('avatar', $user->avatar)"  autofocus autocomplete="avatar" />
             <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
         </div>
         <div class="flex items-center gap-4 mt-4">
